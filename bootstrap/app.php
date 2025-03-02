5da7d0148ef684583a76886f561b2b6918b8366b
Ehsan Mohammadi <ehsanx64@gmail.com>
2025-03-02 16:21:01 +0330

    refactor: rename public folder to public_html

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$theApp = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -16,3 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+$theApp->usePublicPath(dirname(__DIR__) . "/public_html");
+
+return $theApp;
